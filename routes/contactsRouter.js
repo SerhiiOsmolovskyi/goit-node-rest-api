1bec746ec4d51a7bb4ee640f633fad49d9336e28
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2024-05-20 11:50:17 -0600

    corrected endpoint

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -24,7 +24,7 @@ contactsRouter.post("/", isEmptyBody, createContact);
 contactsRouter.put("/:id", validateContactId, isEmptyBody, updateContact);
 
 contactsRouter.patch(
-  "/:id",
+  "/:id/favorite",
   validateContactId,
   isEmptyBody,
   updateStatusContact
